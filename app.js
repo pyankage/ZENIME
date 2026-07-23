780ac3aa3a5803be13589e2391bd2a38289af8bd
pyankage <pyankage@gmail.com>
2026-07-24 01:00:09 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-console.log("ZENIME BERHASIL BERJALAN");
+const animeList = document.getElementById("anime-list");
+const search = document.getElementById("search");
+
+async function loadAnime(keyword = "naruto") {
+    animeList.innerHTML = "<h2 style='color:white'>Loading...</h2>";
+
+    try {
+        const res = await fetch(`https://api.jikan.moe/v4/anime?q=${keyword}&limit=20`);
+        const data = await res.json();
+
+        animeList.innerHTML = "";
+
+        data.data.forEach(anime => {
+            animeList.innerHTML += `
+                <div class="card">
+                    <img src="${anime.images.jpg.large_image_url}" alt="${anime.title}">
+                    <h3>${anime.title}</h3>
+                    <p>⭐ ${anime.score ?? "-"}</p>
+                </div>
+            `;
+        });
+
+    } catch (e) {
+        animeList.innerHTML = "<h2 style='color:red'>Gagal memuat anime.</h2>";
+        console.log(e);
+    }
+}
+
+loadAnime();
+
+search.addEventListener("keyup", () => {
+    const keyword = search.value.trim();
+
+    if (keyword.length >= 3) {
+        loadAnime(keyword);
+    }
+
+    if (keyword.length === 0) {
+        loadAnime();
+    }
+});
